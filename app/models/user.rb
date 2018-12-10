7286b1e9d4857deb25f5c914d63058b6e81c9ffe
Manuel Sousa <mlrcbsousa@gmail.com>
2018-12-10 11:14:31 +0000

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+         :recoverable, :rememberable, :validatable
+
+  devise :omniauthable, omniauth_providers: %i[facebook]
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -257,7 +257,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, "APP_ID", "APP_SECRET", token_params: { parse: :json }
+  config.omniauth :facebook, FACEBOOK_APP_ID, FACEBOOK_APP_SECRET, token_params: { parse: :json }
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
