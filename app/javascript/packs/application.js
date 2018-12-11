57165dc407cd76ffbc9d5b0391b9e1a2b51a65c0
chris sisserian <c.sisserian@gmail.com>
2018-12-11 10:54:01 +0000

    mornings work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+import { initChart } from '../components/chart';
+require Chart.bundle
+require chartkick
+
+const showChart = document.getElementById("burndown");
+  if (showChart) {
+    initChart();
+};
