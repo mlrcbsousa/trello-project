619e231d2366e3ae3dc56e6b9c19eff63461c9b5
Manuel Sousa <mlrcbsousa@gmail.com>
2018-12-11 19:31:04 +0000

    pick sprint route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
+  get 'pick_sprint' to: 'sprints#pick_sprint'
   resources :sprints, only: %i[new]
 
   resource :trello_webhooks, only: %i[show create], defaults: { formats: :json }
