eb572f1fc1695ce4cff8c3b3fbf02b1508ffd015
Manuel Sousa <mlrcbsousa@gmail.com>
2018-12-11 00:15:23 +0000

    scss bootstrap fonts and colors and basic components

--- sandbox.rb
+++ sandbox.rb
@@ -7,4 +7,42 @@ Trello.configure do |config|
   config.oauth_token_secret = User.first.secret
 end
 
+# toget all the classes in a module
 # Trello.constants.select { |c| Trello.const_get(c).is_a? Class }
+
+# all the classes from the command above, part of the Trello module in the ruby-trello gem
+
+trello_module_classes = [
+  :List,
+  :Error,
+  :Action,
+  :Request,
+  :ConfigurationError,
+  :Configuration,
+  :Label,
+  :Comment,
+  :Client,
+  :Token,
+  :Attachment,
+  :Association,
+  :AssociationProxy,
+  :CoverImage,
+  :BasicData,
+  :Board,
+  :Card,
+  :Checklist,
+  :CustomField,
+  :CustomFieldItem,
+  :CustomFieldOption,
+  :Item,
+  :CheckItemState,
+  :LabelName,
+  :Member,
+  :MultiAssociation,
+  :Notification,
+  :Organization,
+  :PluginDatum,
+  :TInternet,
+  :Webhook,
+  :InvalidAccessToken
+]
