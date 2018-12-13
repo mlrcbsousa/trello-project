b0bb1ecd6ade0e579aaf8b227dd3b9b5753ea8be
chris sisserian <c.sisserian@gmail.com>
2018-12-13 18:43:15 +0000

    knowledge, pricing & about pages first draft done, tinkered with some css and navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,17 @@
 # app/controllers/pages_controller.rb
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
-  layout 'home', only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :knowledge, :pricing, :about]
+  layout 'home', only: [:home, :knowledge, :pricing, :about]
 
   def home
   end
 
+  def knowledge
+  end
 
+  def pricing
+  end
+
+  def about
+  end
 end
